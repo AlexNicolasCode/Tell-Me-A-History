f1a4f8724d929a2cdeeb85d101b58eede4bad973
Alex <alexoliveira604@gmail.com>
2021-05-09 17:52:21 -0300

    integration with history folder added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-
 const file = require("./data/data.json");
+
+bot.on("message", msg => {
+    if (msg.content == "!tellme batman") {
+      const newMsg = msg.content.substr(8);
+      const data = require(`./history/${newMsg}`);
+      msg.channel.send(data);
+      console.log("New message was been sended");
+    }
+});
 
 bot.on("message", msg => {
   if (msg.content === "!tellme flash") {
