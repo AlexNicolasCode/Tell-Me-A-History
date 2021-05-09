d30e832f947ea783897ce53cb39814ae8f849e31
Alex <alexoliveira604@gmail.com>
2021-05-09 03:38:31 -0300

    first messages system

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const file = require("./data/data.json");
+
+bot.on("message", msg => {
+  if (msg.content === "!tellme flash") {
+    msg.channel.send(file.flash)
+    console.log("New message was been sended")
+  }
+});
+
+bot.on("message", msg => {
+  if (msg.content === "!tellme lol") {
+    msg.channel.send(file.lol)
+    console.log("New message was been sended")
+  }
+});
+
+bot.on("message", msg => {
+  if (msg.content === "!tellme jackson") {
+    msg.channel.send(file.jackson)
+    console.log("New message was been sended")
+  }
+});
+
+bot.login("TOKEN");
