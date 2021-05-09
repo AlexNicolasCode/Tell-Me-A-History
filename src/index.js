63406851d52e6cb8c2a4f1bfe0a1601210ce2876
Alex <alexoliveira604@gmail.com>
2021-05-09 18:05:04 -0300

    message send optimization

--- src/index.js
+++ src/index.js
@@ -3,33 +3,12 @@ const bot = new Discord.Client();
 const file = require("./data/data.json");
 
 bot.on("message", msg => {
-    if (msg.content == "!tellme batman") {
+    if (msg.content.substr(0, 7) == "!tellme") {
       const newMsg = msg.content.substr(8);
       const data = require(`./history/${newMsg}`);
       msg.channel.send(data);
       console.log("New message was been sended");
     }
-});
-
-bot.on("message", msg => {
-  if (msg.content === "!tellme flash") {
-    msg.channel.send(file.flash)
-    console.log("New message was been sended")
-  }
-});
-
-bot.on("message", msg => {
-  if (msg.content === "!tellme lol") {
-    msg.channel.send(file.lol)
-    console.log("New message was been sended")
-  }
-});
-
-bot.on("message", msg => {
-  if (msg.content === "!tellme jackson") {
-    msg.channel.send(file.jackson)
-    console.log("New message was been sended")
-  }
 });
 
 bot.login("TOKEN");
