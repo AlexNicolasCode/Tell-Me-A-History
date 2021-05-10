0592b239cf3260bb8a2046ef1e3d7241b6b3e06d
Alex <alexoliveira604@gmail.com>
2021-05-09 21:21:16 -0300

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ const fs = require("fs")
 
 bot.on("message", msg => {
     if (msg.content.substr(0, 7) == "!tellme") {
-      const newMsg = msg.content.substr(8);
+      const newMsg = msg.content.substr(8).toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+      console.log(newMsg)
       const folderPath = `./history/${newMsg}.json`;
 
       if (fs.existsSync(folderPath)) {
         const data = JSON.parse(fs.readFileSync(folderPath));
         const content = new MessageEmbed()
-          .setTitle(newMsg[0].toUpperCase() + newMsg.substring(1))
+          .setTitle(data.title)
           .setColor(0xff0000)
           .setDescription(data.summary)
           .setImage(data.image)
@@ -28,23 +29,24 @@ bot.on("message", msg => {
 
             const page = await wiki.page(newMsg);
             const summary = await page.summary();
-            const title = await summary.title.toLowerCase();
+            const fileName = await newMsg.toLowerCase();
             const arr = 
             {
+                title: summary.title,
                 summary: summary.extract,
                 url: page.fullurl,
                 image: summary.thumbnail.source
             }
 
-            jsonfile.writeFile(`./history/${title}.json`, arr, function (err) {
-              console.log(`${title}.json has been created!`)
+            jsonfile.writeFile(`./history/${fileName}.json`, arr, function (err) {
+              console.log(`${fileName}.json has been created!`)
               if (err) console.error(err)
             })
 
             setTimeout(() => {
               const data = JSON.parse(fs.readFileSync(folderPath));
               const content = new MessageEmbed()
-                .setTitle(newMsg[0].toUpperCase() + newMsg.substring(1))
+                .setTitle(data.title)
                 .setColor(0xff0000)
                 .setDescription(data.summary)
                 .setImage(data.image)
@@ -52,7 +54,7 @@ bot.on("message", msg => {
 
               msg.channel.send(content);
               console.log("New message was been sended");
-            }, 100)
+            }, 5)
 
           } catch (error) {
             console.log(error);
@@ -62,5 +64,4 @@ bot.on("message", msg => {
     }
 });
 
-
-bot.login("TOKEN");
+bot.login("ODMxMDMwMzY4NjkzNzgwNTEw.YHPTVw.9vkcP7K3BkFrAfwUPcjqeQv-uRY");
